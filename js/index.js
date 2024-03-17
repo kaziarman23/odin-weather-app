f374cba3bf4f5ab95c2f99ef92762294a9a7e1f9
kaziarman23 <kaziarman@proton.me>
2024-03-17 20:05:20 +0600

    called api success

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-const apiKey = "9cbed465b252411683100543241703";
+const APIkey = "b64528a0db0ba8adfe77ed62ad0af4a6";
+const city = "barlin";
+async function call() {
+    const raw = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`
+    );
+    const ans = await raw.json();
+    console.log(ans)
+}
+
+
+call()
